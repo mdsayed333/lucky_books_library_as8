a226ca8a5459d441617981ff5c5ef2e75281dda7
Sayed <mdsayedgv@gmail.com>
2022-03-25 22:00:04 +0600

    Create json data and component

---
+++ src/Components/LuckyBooks/LuckyBooks.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LuckyBooks = () => {
+    return (
+        <div>
+            <div>
+                <h1>Library</h1>
+            </div>
+            <div>
+                <h1>cart..</h1>
+            </div>
+        </div>
+    );
+};
+
+export default LuckyBooks;
